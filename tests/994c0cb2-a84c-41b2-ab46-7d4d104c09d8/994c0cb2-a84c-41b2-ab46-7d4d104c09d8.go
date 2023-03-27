22f838c6e398e2b7f6e5446124963e22a45690ab
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-03-27 15:59:33 -0400

    VST: Adding QuakBot malicious OneNote (#104)
    * Add files via upload
    
    * minor tweak

---
+++ tests/994c0cb2-a84c-41b2-ab46-7d4d104c09d8/994c0cb2-a84c-41b2-ab46-7d4d104c09d8.go
@@ -0,0 +1,30 @@
+/*
+ID: 994c0cb2-a84c-41b2-ab46-7d4d104c09d8
+NAME: Will your computer quarantine QuakBot malware?
+CREATED: 2023-03-27
+*/
+package main
+
+import (
+	_ "embed"
+
+	Endpoint "github.com/preludeorg/test/endpoint"
+)
+
+//go:embed quakbot.one
+var malicious []byte
+
+func test() {
+	println("[+] Extracting file for quarantine test")
+	println("[+] Pausing for 1 second to gauge defensive reaction")
+	if Endpoint.Quarantined("quakbot.one", malicious) {
+		println("[+] Malicious file was caught!")
+		Endpoint.Stop(105)
+	}
+	println("[-] Malicious file was not caught")
+	Endpoint.Stop(101)
+}
+
+func main() {
+	Endpoint.Start(test)
+}
