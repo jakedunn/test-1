f4862b9a6764407639bb26c5daf5a489f7f3821c
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-05-03 10:27:46 -0400

    Add files via upload (#125)

---
+++ tests/db201110-d875-4133-9709-2732a47f252f/db201110-d875-4133-9709-2732a47f252f.go
@@ -0,0 +1,79 @@
+/*
+ID: db201110-d875-4133-9709-2732a47f252f
+NAME: Can you stop a common ransomware attempt?
+CREATED: 2023-01-03
+*/
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+
+	Endpoint "github.com/preludeorg/test/endpoint"
+)
+
+func Encrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext, nil
+}
+
+func GenerateKey() ([]byte, error) {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func test() {
+
+	println("[*] Generating sample files...")
+	Endpoint.Write("one.txt", make([]byte, 1000))
+	Endpoint.Write("two.xlsx", make([]byte, 7500000))
+	Endpoint.Write("three.pdf", make([]byte, 5500))
+
+	key, err := GenerateKey()
+	if err != nil {
+		println(err)
+		Endpoint.Stop(1)
+	}
+	println("[+] Generated a new encryption key")
+
+	arr := [3]string{"one.txt", "two.xlsx", "three.pdf"}
+	for _, name := range arr {
+		println("[+] Starting encryption process: " + name)
+		bytesRead := Endpoint.Read(name)
+
+		encryptBytes, err := Encrypt(key, bytesRead)
+		if err != nil {
+			println(err)
+		}
+
+		Endpoint.Write(name, encryptBytes)
+	}
+
+	Endpoint.Stop(101)
+}
+
+func main() {
+	Endpoint.Start(test)
+}
